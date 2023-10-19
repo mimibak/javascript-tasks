93ccceb8d9fec9d515679a99dba9403210bb9b33
Mimi Bak <bak.mihyeong@gmail.com>
2023-10-19 08:20:12 +0200

    feat:add-operators-n-datatypes-task

---
+++ 02_ct-operators-n-datatypes/script.js
@@ -0,0 +1,31 @@
+// Thats my first name
+console.log("M" + "i" + "h" + "-" + "Y" + "e" + "o" + "n" + "g");
+
+// 02-Spot the problems & solve them
+
+// Error! Make it work
+console.log("Hallo");
+
+// Error! Make it work
+console.log("That doesn't work");
+
+// should return true. Change only the operator to fix it.
+console.log(50 + 3 > 52);
+
+// should be false. Change only the operator to fix it.
+console.log("333" !== 333);
+
+// Wrong result: Expected 555 not 855. Change only the operator to fix it.
+console.log((600 + 510) / 2);
+
+// Wrong result: Expected a Number 4 received 22
+console.log(2 + 2);
+
+// 03-Compare two conditions
+//  Create a console.log that returns true when both division results are equal to 5
+
+console.log(25 / 5); // Result: 5
+console.log(50 / 10); // Result: 5
+
+console.log(25 / 5 >= 5);
+console.log(50 / 10 >= 5);
